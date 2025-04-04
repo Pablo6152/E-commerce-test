4d01911c6b231ce85c2cee24ab270d5280706f80
Rain <rainkalugdan@gmail.com>
2025-04-04 11:17:28 +0800

    chore: cache DOM elements and initialize global cart and product data

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Global Cart Array
+const cartItems = [];
+
+// DOM Element References
+const searchForm = document.querySelector('[data-js="search-form"]');
+const searchInput = document.querySelector('[data-js="search-input"]');
+const cartBtn = document.querySelector('[data-js="cart-btn"]');
+const cartCount = document.querySelector('[data-js="cart-count"]');
+const productGrid = document.querySelector(
+  '[data-js="product-grid-container"]'
+);
+
+const productsData = [];
