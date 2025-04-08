7546763d5b354a1510d3a7347d2746e3bf2b6b8b
Rain <rainkalugdan@gmail.com>
2025-04-08 13:22:03 +0800

    feat(product-card): add function to generate product card markup

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,28 @@ let state = {
   },
 };
 
+// Generates a single product card markup
+function createProductCardMarkup(product) {
+  return `
+  <article class="product-card"> 
+  <div class="product-card__header">
+  <img src="${product.image}" 
+  alt="${product.name}" 
+  class="product-card__image"/>
+  <button class="add-to-cart-btn" 
+  data-product-id="${product.id}" 
+  data-product-name="${product.name}"
+  data-product-price="${product.price}"
+  aria-label="Add ${product.name} to Cart"><img src="./assets/icons/cart.svg" alt="" aria-hidden="true"/> 
+  </button>
+  </div>
+  <div class="product-card__footer">
+  <h3>${product.name}</h3>
+  <span class="fw-semi-bold" aria-label="Price: ${product.price}">${product.price}</span>
+  </div> 
+  </article>`;
+}
+
 // Loads product data from JSON and updates state
 async function loadProducts() {
   try {
