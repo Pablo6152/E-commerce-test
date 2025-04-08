8c1f70395332712b41f735d603cac80b961d09a2
Rain <rainkalugdan@gmail.com>
2025-04-08 13:19:54 +0800

    refactor(DOM references): remove DOM references

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,6 @@ let state = {
   },
 };
 
-// DOM References
-const DOM = {
-  searchForm: document.querySelector('[data-js="search-form"]'),
-  searchInput: document.querySelector('[data-js="search-input"]'),
-  cartBtn: document.querySelector('[data-js="cart-btn"]'),
-  cartCount: document.querySelector('[data-js="cart-count"]'),
-};
-
 // Loads product data from JSON and updates state
 async function loadProducts() {
   try {
